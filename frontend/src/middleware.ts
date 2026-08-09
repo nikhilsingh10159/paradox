1b2eae441b986bcd62c77b51dc1b8e7ef735fc96
satvikjoshi17 <satvikjoshi9thc@gmail.com>
2026-08-09 22:24:52 +0530

    functional commit

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,6 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/_next')) {
+    return NextResponse.next();
+  }
   return NextResponse.next();
 }
 
